a826499be6eb48bc8fca7134dbc280489864f981
BADAL017 <102615409+BADAL017@users.noreply.github.com>
2022-03-30 12:23:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-exports.TOKEN = "your bot token";
+exports.TOKEN = "OTU4NjE5MzA3NTMxNzE0NTcw.YkP9zQ.rSR_Bfv2Jx_wBgR1xBuCeRwV-3A";
 
-exports.PREFIX = 'YOUR PREFIX';
+exports.PREFIX = '₹';
 
-exports.OWNER_ID = "YOUR ACC ID"
+exports.OWNER_ID = "685119672399691828"
